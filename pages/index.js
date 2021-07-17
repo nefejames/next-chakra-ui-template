0a297ada2986c12cdfd0b38124430658df51bf5d
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-17 10:47:59 -0700

    made changes

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Next Chakra UI Template</title>
       </Head>
       <Text>Let&apos;s go</Text>
     </div>
